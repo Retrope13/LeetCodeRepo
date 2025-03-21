bc38998515c0dc90345582ed829134940097d280
Samuel Mckay <89430082+Retrope13@users.noreply.github.com>
2025-03-21 13:07:43 -0600

    Time: 0 ms (100.00%) | Memory: 58.2 MB (17.95%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -4,21 +4,24 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let l = 0, r = nums.length - 1
+        let l = Math.floor((nums.length - 1) / 2), r = nums.length - 1
 
-    while (l <= r) {
-        if (nums[l] < target) {
-            l++
-        } else if (nums[l] == target) {
-            return l
+        while (l <= r) {
+            if (nums[l] > target) {
+                r = l;
+                l = 0;
+            } else if (nums[l] < target) {
+                l++;
+            } else if (nums[l] == target) {
+                return l;
+            }
+
+            if (nums[r] > target) {
+                r--;
+            } else if (nums[r] == target) {
+                return r
+            }
         }
 
-        if (nums[r] > target) {
-            r--
-        } else if (nums[r] == target) {
-            return r
-        }
-    }
-
-    return -1
+        return -1
 };
